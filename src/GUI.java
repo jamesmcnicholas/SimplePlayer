7ae86954bb6b2a6032c5923477fc0f12e4d00c3d
James McNicholas <jmcnicholas.62743@farnborough.ac.uk>
2017-10-22 18:33:21 +0100

    Set up scene switching

--- src/GUI.java
+++ src/GUI.java
@@ -1,7 +1,11 @@
+import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.*;
 import javafx.application.Application;
 import javafx.stage.Stage;
+
+import java.io.IOException;
+
 
 public class GUI extends Application{
     @Override
@@ -15,4 +19,18 @@ public class GUI extends Application{
         stage.show();
     }
     public static void main(String args[]){launch(args);}
+
+    public void login(ActionEvent event) throws Exception{
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("mainScreen.fxml"));
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root, 1280, 720));
+            stage.setTitle("Main Screen");
+            stage.show();
+            ((Node) (event.getSource())).getScene().getWindow().hide();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
+    }
 }
--- src/controller.java
+++ src/controller.java
@@ -1,16 +1,43 @@
 import javafx.fxml.FXML;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class controller {
+
+    private GUI gui;
+
+    public void setGui(GUI gui){
+        this.gui=gui;
+    }
     @FXML private TextField usernameField;
     @FXML private TextField passwordField;
 
-    @FXML protected void loginButtonPress(ActionEvent event){
-        System.out.println("login PRESSED");
-    }
+    @FXML protected void loginButtonPress(ActionEvent event){ System.out.println("login pressed"); }
     @FXML protected void forgotPasswordButtonPress(ActionEvent event){
         System.out.println("forgot PRESSED");
     }
-    @FXML protected void createAccountButtonPress(ActionEvent event){System.out.println("accout PRESSED");}
+    @FXML protected void createAccountButtonPress(ActionEvent event){System.out.println("account PRESSED");}
+
+    //Method to change the scene to the main screen
+    public void changeSceneButtonPushed(ActionEvent event) throws IOException {
+        Parent mainScreenParent = FXMLLoader.load(getClass().getResource("mainScreen.fxml"));
+        Scene mainScreenScene = new Scene(mainScreenParent);
+
+        //This line gets the stage info
+        Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
+
+        window.setScene(mainScreenScene);
+        window.show();
+    }
+    @FXML protected void nextButtonPressed(ActionEvent event){ System.out.println("skip pressed"); }
+    @FXML protected void prevButtonPressed(ActionEvent event){ System.out.println("prev pressed"); }
+    @FXML protected void pauseButtonPressed(ActionEvent event){ System.out.println("pause pressed"); }
+
 }
