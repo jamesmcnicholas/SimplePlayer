29a6d29564ae80baff4903de0de8cbd5d2df627b
James McNicholas <jmcnicholas.62743@farnborough.ac.uk>
2017-10-29 17:11:05 +0000

    Entirely remodeled main screen, added more placeholder buttons

--- src/controller.java
+++ src/controller.java
@@ -39,5 +39,10 @@ public class controller {
     @FXML protected void nextButtonPressed(ActionEvent event){ System.out.println("skip pressed"); }
     @FXML protected void prevButtonPressed(ActionEvent event){ System.out.println("prev pressed"); }
     @FXML protected void pauseButtonPressed(ActionEvent event){ System.out.println("pause pressed"); }
+    @FXML protected void queueButtonPressed(ActionEvent event){ System.out.println("queued song"); }
+    @FXML protected void addSongButtonPressed(ActionEvent event){ System.out.println("add song pressed"); }
+    @FXML protected void removeSongButtonPressed(ActionEvent event){ System.out.println("song remove pressed"); }
+    @FXML protected void playNextButtonPressed(ActionEvent event){ System.out.println("song will play next"); }
+    @FXML protected void addToPlaylistButtonPressed(ActionEvent event){ System.out.println("added to playlist"); }
 
 }
